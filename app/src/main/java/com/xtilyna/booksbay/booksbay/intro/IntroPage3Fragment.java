884be3e8c08d4105f3a3b5d05f1d589ca1b3c03b
Christilyn A <christilynarjona@outlook.com>
2018-03-07 22:37:24 -0700

    intro activity added

---
+++ app/src/main/java/com/xtilyna/booksbay/booksbay/intro/IntroPage3Fragment.java
@@ -0,0 +1,30 @@
+package com.xtilyna.booksbay.booksbay.intro;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xtilyna.booksbay.booksbay.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IntroPage3Fragment extends Fragment {
+
+
+    public IntroPage3Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro_page3, container, false);
+    }
+
+}
